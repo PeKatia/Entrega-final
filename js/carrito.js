476d5d89ec3ce68418c084fecddd282f1f73cea0
PeKatia <katutuuu@gmail.com>
2025-07-13 23:11:48 -0300

    se agrego vaciarProductos y modificarCantidad

--- js/carrito.js
+++ js/carrito.js
@@ -43,16 +43,17 @@ const mostrarCarrito = () => {
             <span>$${item.precio} </span>
           </div>
             <div class="cantidad">
-              <button class="btn-menos">&minus;</button>
-              <input type="number" value="${item.cantidad}" min="1" max="99">
-              <button class="btn-mas">&plus;</button>
+              <button data-id=${item.id} data-value="-1" class="btn-menos">&minus;</button>
+              <input id="cantidad-${item.id}"  type="number" disabled
+              value="${item.cantidad}" min="1" max="99">
+              <button data-id=${item.id} data-value="1" class="btn-mas">&plus;</button>
             </div>           
           <div>
             <p>Precio</p>
             <p>$${(item.precio * item.cantidad).toFixed(2)}</p>
           </div>
-          <button data-id=${item.id}
-            type="button" class="btn btn-primary btn-pink boton-eliminar">&#10006;</button>
+          <button data-id=${item.id} type="button" 
+          class="btn btn-primary btn-pink boton-eliminar">&#10006;</button>
         </div>
         `;
     // agrego el article "producto" al contenedor
@@ -60,8 +61,29 @@ const mostrarCarrito = () => {
 
     const botonEliminar = producto.querySelector(".boton-eliminar");
     botonEliminar.addEventListener("click", eliminarProducto);
+
+    const botonMenos = producto.querySelector(".btn-menos");
+    botonMenos.addEventListener("click", modificarCantidad);
+
+    const botonMas = producto.querySelector(".btn-mas");
+    botonMas.addEventListener("click", modificarCantidad);
   });
 };
+  const modificarCantidad = (event) => {
+   let btn = event.target;
+   let id = btn.getAttribute("data-id");
+   let valor = btn.getAttribute("data-value");
+   const cantidad = document.getElementById("cantidad-" + id);
+   cantidad.value = parseInt(cantidad.value) + parseInt(valor);
+   const validarId = (item) => item.id == id;
+   const indiceCarrito = carrito.findIndex(validarId);
+   carrito[indiceCarrito].cantidad = cantidad.value;
+   localStorage.setItem("carrito", JSON.stringify(carrito));
+
+   mostrarTotalCompra();
+
+
+ };
 
 const calcularPrecioCompra = () => {
   return carrito
@@ -80,10 +102,16 @@ const calcularCantidadCompra = () => {
     0
   );
 };
+const vaciarProductos = () => {
+  const listaArticulos = document.querySelector(".contenedor-carrito");
+  listaArticulos.innerHTML = `Tu carrito esta vacio`;
+  let contenedorCarrito = document.querySelector(".resumen-carrito");
+  contenedorCarrito.style.display = "none";
+  localStorage.removeItem("carrito");
+};
 
 const mostrarTotalCompra = () => {
   let contenedorCarrito = document.querySelector(".resumen-carrito");
-
   if (carrito.length === 0) {
     contenedorCarrito.style.display = "none";
     return;
@@ -101,14 +129,18 @@ const mostrarTotalCompra = () => {
       <button type="submit" class="btn btn-primary btn-pink">
         Finalizar compra
       </button>
-      <button type="button" class="btn btn-primary btn-pink">
+      <button   type="button" class="btn btn-primary btn-pink btn-vaciarCarrito" >
         Vaciar carrito
       </button>
-
+  
       `;
 
   // muestro elemento resumen carrito
   contenedorCarrito.style.display = "block";
+
+  const botonVaciarCarrito =
+    contenedorCarrito.querySelector(".btn-vaciarCarrito");
+  botonVaciarCarrito.addEventListener("click", vaciarProductos);
 };
 
 mostrarCarrito();
